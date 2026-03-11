195efcef80060b488dc069f9e6fe6d90b0b04f98
Jean-AT <jeanpoolariass2020@hotmail.com>
2026-03-11 17:33:12 -0500

    Add HttpOnly

--- src/main/java/com/service/command/config/ConfigAcces.java
+++ src/main/java/com/service/command/config/ConfigAcces.java
@@ -1,5 +1,10 @@
 package com.service.command.config;
 
+import com.service.command.users.models.Users;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,21 +12,63 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
 @Configuration
 public class ConfigAcces {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF para probar APIs fácil
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/**").permitAll()
                 );
 
         return httpSecurity.build();
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    private static final String MI_CLAVE_SECRETA = "EstaEsUnaClaveSuperSecretaYMuyLargaParaGarantizarLaSeguridadDelJwtGozu123456";
+    private static final String ADMINKEY = "ASASFJ2IJF0248FN23T203880M2@235209D0J90";
+    private final Key key = Keys.hmacShaKeyFor(MI_CLAVE_SECRETA.getBytes(StandardCharsets.UTF_8));
+    private final Key Adminkey = Keys.hmacShaKeyFor(ADMINKEY.getBytes(StandardCharsets.UTF_8));
+    private final long expirationTime = 21600000;
+
+    public String generateToken(Users user) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expirationTime);
+
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .claim("id", user.getId())
+                .claim("name", user.getName())
+                .claim("lastName", user.getLastName())
+                .claim("role", user.getRol().name())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(key)
+                .compact();
+    }
+
+    public Claims getAllClaimsFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
